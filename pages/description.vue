0fb44dfa37b701ea482dca9e501e9f9b788cbe85
crisandana <104160030+crisandana@users.noreply.github.com>
2023-07-14 21:08:02 +0200

    Add files via upload

---
+++ pages/description.vue
@@ -0,0 +1,15 @@
+<template>   
+  <div> {{description}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            description: "The task itself wasn't hard, but for me where some new things. For example a nuxt link, the first half part I understand, but the second not so much, that's why I haven't finish it and with the star I have also some issues",
+        };
+    },  
+
+};
+</script>
+
