c167ce90785d3f7ef051b2a488924dbe279943f6
crisandana <104160030+crisandana@users.noreply.github.com>
2023-07-14 21:16:54 +0200

    Update description.vue

--- pages/description.vue
+++ pages/description.vue
@@ -6,7 +6,7 @@
 export default {
     data() {
         return {
-            description: "The task itself wasn't hard, but for me where some new things. For example a nuxt link, the first half part I understand, but the second not so much, that's why I haven't finish it and with the star I have also some issues",
+            description: "The task itself wasn't hard, but for me where some new things. For example a nuxt link, the first half part I understand, but the second half not so much, that's why I haven't finish it and with the star I have also some issues. Still, even with so many problems, I wish I had more time to work them out, and maybe to ask where I am stuck, because as always I stuck at some point and I can't go forther until is finish!",
         };
     },  
 
