61a1657f7026d3f39a3e6250f010ec9f5aebdf0c
crisandana <104160030+crisandana@users.noreply.github.com>
2023-07-15 02:42:46 +0200

    Update Card.vue

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,9 @@
 <template>
-	<div :class="`card ${card.done ? 'is-complete' : ''}`">
+	<div :class="`card`">
 		<div class="content">{{ card.serviceName }}</div>
         <div class="content">{{ card.businessName }}</div>
         <div class="content">{{ card.rating }}</div>
 		<div class="buttons">
-			<button @click="toggleCard">{{ card.done ? 'Undo' : 'Done' }}</button>
 			<button @click="removeCard" class="delete">Delete</button>
 		</div>
 	</div>
@@ -14,9 +13,6 @@
 export default {
 	props: ['card'],
 	methods: {
-		toggleCard() {
-			this.$store.commit('TOGGLE_CARD', this.card);
-		},
 		removeCard() {
 			this.$store.commit('REMOVE_CARD', this.card);
 		}
